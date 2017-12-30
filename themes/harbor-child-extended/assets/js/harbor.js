b87169959b3ae71772ed841434563dc641aabcae
ruucm-working <ruucm.a@gmail.com>
2017-12-31 00:17:07 +0900

    new init code of aos

--- themes/harbor-child-extended/assets/js/harbor.js
+++ themes/harbor-child-extended/assets/js/harbor.js
@@ -3,14 +3,35 @@
 /**
  *	Aos Init
  */
-AOS.init({
-	startEvent: 'load',
-	once: true,
-});
-jQuery(function($){
-	AOS.refresh();
-});
+// AOS.init({
+// 	startEvent: 'load',
+// 	once: true,
+// });
+// jQuery(function($){
+// 	AOS.refresh();
+// });
+// window.addEventListener('load', AOS.refresh);
+
+
+// $(window).on('resize', function () { AOS.refresh(); });
+// $(window).on('load', function() { setTimeout(AOS.refreshHard, 150); });
+
+// $(document).ready(function () {
+//   AOS.init({ 
+//    startEvent: 'load', 
+//    easing: 'ease-in-out-quart', 
+//    duration: 600,  once: false });            
+// });
+
+window.addEventListener('resize', AOS.refresh);
 window.addEventListener('load', AOS.refresh);
+jQuery(function($){
+  AOS.init({ 
+   startEvent: 'load', 
+   easing: 'ease-in-out-quart', 
+   duration: 600,  once: false });            
+});
+
 /**
  *	Mojs Animation
  */
